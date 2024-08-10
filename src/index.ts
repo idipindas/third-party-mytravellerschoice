7cde05fdae7b6aa6de7c3baed4f0c3f4031e2acc
dipindas-a-h <dipindasofficial@gmail.com>
2024-08-10 10:00:39 +0530

    index

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,25 @@
-import express, { Request, Response } from 'express';
-import dotenv from 'dotenv';
-import router from './routes/routes';
-import morgan from 'morgan'
-import cors from 'cors'
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import router from "./routes/routes";
+import morgan from "morgan";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const port = 3000;
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cors());
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
 // A sample route
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript with Express!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, TypeScript with Express!");
 });
-app.use('/api/v1',router)
-
+app.use("/api/v1", router);
 
 // Start the server
 app.listen(process.env.PORT, () => {
