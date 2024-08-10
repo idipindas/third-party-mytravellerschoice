9959db3d5a660c7497e73837113b91e63fec1f22
dipindas-a-h <dipindasofficial@gmail.com>
2024-08-10 16:18:24 +0530

    testinh

--- src/services/apiTicketService.ts
+++ src/services/apiTicketService.ts
@@ -7,7 +7,7 @@ export const apiTicketService = ()=>{
         console.log('t1',data)
 
         try{
-            const resp =await  axios.post(`${BASE_URL}/getAgentDetails`,data)
+            const resp =await  axios.post(`https://parmartours.com:8443/parmartour/Test/getAgentDetails`,data)
             console.log('t2',data,resp)
             return resp
 
