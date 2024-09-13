dad48ab70aadf332acec37bfa39a6c1064a3809b
dipindas-a-h <dipindasofficial@gmail.com>
2024-09-13 16:47:36 +0530

    change

--- src/validator/confirmBookingValidator.ts
+++ src/validator/confirmBookingValidator.ts
@@ -10,16 +10,12 @@ export const confirmBookingValidator = Joi.object({
     "string.min": "userPassword should be at least 8 characters long",
     "any.required": "userPassword is required",
   }),
-  attractionId: Joi.number().integer().positive().required().messages({
-    "number.base": "attractionId must be a number",
-    "number.integer": "attractionId must be an integer",
-    "number.positive": "attractionId must be a positive number",
+  attractionId: Joi.required().messages({
+   
     "any.required": "attractionId is required",
   }),
-  ticketTypeId: Joi.number().integer().positive().required().messages({
-    "number.base": "ticketTypeId must be a number",
-    "number.integer": "ticketTypeId must be an integer",
-    "number.positive": "ticketTypeId must be a positive number",
+  ticketTypeId: Joi.required().messages({
+   
     "any.required": "ticketTypeId is required",
   }),
   customerName: Joi.string().required().messages({
@@ -37,16 +33,12 @@ export const confirmBookingValidator = Joi.object({
         "customerPhoneNumber must be a valid 10-digit number",
       "any.required": "customerPhoneNumber is required",
     }),
-  nofAdult: Joi.number().integer().positive().required().messages({
-    "number.base": "nofAdult must be a number",
-    "number.integer": "nofAdult must be an integer",
-    "number.positive": "nofAdult must be a positive number",
+  nofAdult: Joi.required().messages({
+    
     "any.required": "nofAdult is required",
   }),
-  nofChild: Joi.number().integer().min(0).required().messages({
-    "number.base": "nofChild must be a number",
-    "number.integer": "nofChild must be an integer",
-    "number.min": "nofChild cannot be negative",
+  nofChild: Joi.required().messages({
+   
     "any.required": "nofChild is required",
   }),
   travelDate: Joi.date().iso().required().messages({
